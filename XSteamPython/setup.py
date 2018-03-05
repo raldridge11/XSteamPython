934fd98ecffdb2c3d5a860664be32855c08a649c
Bobby Aldridge <aldridge.robert.james@gmail.com>
2018-03-04 22:01:08 -0500

    Adds setup.py to write and delete __init__.py.

--- XSteamPython/setup.py
+++ XSteamPython/setup.py
@@ -1,7 +1,12 @@
+import os
 from distutils.core import setup
+
+open('__init__.py', 'a').close()
 
 setup(name='XSteamPython',
       version='0.0.0',
       description='Steam Tables in Python',
       packages=['XSteamPython'],
      )
+
+os.remove('__init__.py')
