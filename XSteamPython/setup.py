b12d702101dda0b358fdbdd9272ae7fdf79fe75c
Bobby Aldridge <aldridge.robert.james@gmail.com>
2018-03-05 00:52:35 -0500

    Updates setup.py to make XSteamPython a module, not package. Modifies travis to install requirements first.

--- XSteamPython/setup.py
+++ XSteamPython/setup.py
@@ -1,12 +1,3 @@
-import os
-from distutils.core import setup
-
-open('__init__.py', 'a').close()
-
+from setuptools import setup
 setup(name='XSteamPython',
-      version='0.0.0',
-      description='Steam Tables in Python',
-      packages=['XSteamPython'],
-     )
-
-os.remove('__init__.py')
+py_modules=['XSteamPython'])
