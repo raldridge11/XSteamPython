e54d8c3fcd1531d9b2c0bc5a83813d93c625552b
Bobby Aldridge <aldridge.robert.james@gmail.com>
2018-03-04 21:57:04 -0500

    Gives a valid version number to setup.py.

--- XSteamPython/setup.py
+++ XSteamPython/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='XSteamPython',
-      version='none',
+      version='0.0.0',
       description='Steam Tables in Python',
       packages=['XSteamPython'],
      )
