118d246e6e05563510262b871383575fe38c7c99
Robert Aldridge <aldridge.robert.james@gmail.com>
2018-07-06 14:34:20 -0700

    Updates #159, adds ExtractSteamData project to extract values in excel files to *npz files for easier handling of test comparison data. Adds data for Tsat_p.

---
+++ ExtractSteamData/ExtractSteamData.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+'''
+* Water and steam properties according to IAPWS IF-97
+* By Magnus Holmgren, www.x-eng.com
+* The steam tables are free and provided as is.
+* We take no responsibilities for any errors in the code or damage thereby.
+* You are free to use, modify and distribute the code as long as authorship is properly acknowledged.
+* Please notify me at magnus@x-eng.com if the code is used in commercial applications
+'''
+import argparse
+
+import numpy as np
+import pandas as pd
+
+def getOneDimensionalTestData(excelData):
+    dataAsMatrix = excelData.as_matrix()
+    return dataAsMatrix[0, 1:], dataAsMatrix[1, 1:]
+
+def getTwoDimensionalTestData(excelData):
+    dataAsMatrix = excelData.as_matrix()
+    return dataAsMatrix[0, 1:], dataAsMatrix[1:, 0], dataAsMatrix[1:, 1:]
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='Spreadsheet data file to read in', required=True)
+    parser.add_argument('-s', help='Sheet in spreadsheet to read in', required=True)
+    args = parser.parse_args()
+
+    data = pd.read_excel(args.f, args.s)
+
+    units = args.f.split('_')[0]
+    dependentVariableName, indep = args.s.split('_')
+    independentXVariableName, independentYVariableName = indep[0], indep[-1]
+
+    if data.shape[0] == 2:
+        independentXVariable, dependentVariable = getOneDimensionalTestData(data)
+        np.savez_compressed('{}_{}'.format(units, args.s), independentXVariableName=independentXVariable, dependentVariableName=dependentVariable)
+    elif data.shape[0] > 2:
+        independentXVariable, independentYVariable, dependentVariable = getTwoDimensionalTestData(data)
+        np.savez_compressed('{}_{}'.format(units, args.s), independentXVariableName=independentXVariable, independentYVariableName=independentYVariable, dependentVariableName=dependentVariable)
+    else:
+        raise(ValueError)
+
+if __name__ == "__main__":
+    main()
--- tests/Data.py
+++ tests/Data.py
@@ -10,24 +10,6 @@
 import numpy as np
 import pandas as pd
 
-try:
-    siData = pd.ExcelFile('SIUnits_testCompare.xlsm')
-    englishData = pd.ExcelFile('EnglishUnits_testCompare.xlsm')
-except Exception:
-    siData = pd.ExcelFile('tests/SIUnits_testCompare.xlsm')
-    englishData = pd.ExcelFile('tests/EnglishUnits_testCompare.xlsm')
-
-def getOneDimensionalTestData(excelFile, excelPage):
-
-    data = pd.read_excel(excelFile, excelPage)
-    dataAsMatrix = data.as_matrix()
-    return dataAsMatrix[0, 1:], dataAsMatrix[1, 1:]
-
-def getTwoDimensionalTestData(excelFile, excelPage):
-    data = pd.read_excel(excelFile, excelPage)
-    dataAsMatrix = data.as_matrix()
-    return dataAsMatrix[0, 1:], dataAsMatrix[1:, 0], dataAsMatrix[1:, 1:]
-
 def calculatePropertyFromOneDimension(function, independentVariable):
     thProperty = np.fromiter((function(x) for x in independentVariable), float)
     return thProperty
