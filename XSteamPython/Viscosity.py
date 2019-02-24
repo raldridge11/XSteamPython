44cab5ea4ef045c2204083b3eeefbdfcc5c4b4a4
Robert Aldridge <aldridge.robert.james@gmail.com>
2019-02-24 15:52:42 -0800

    Closes #149, adds my_allregions_ph and tests.

--- XSteamPython/Viscosity.py
+++ XSteamPython/Viscosity.py
@@ -16,6 +16,7 @@ import Regions
 import Region1
 import Region2
 import Region3
+import Region4
 import Region5
 
 h0 = np.array([0.5132047, 0.3205656, 0.0, 0.0, -0.7782567, 0.1885447])
@@ -53,7 +54,6 @@ def my_allregions_pT(pressure, temperature):
 
     rhos = density/317.63
     ts = temperature/647.226
-    ps = pressure/22.115
 
     my_0 = ts**0.5/(1.0 + 0.978197/ts + 0.579829/(ts**2.0) - 0.202354/(ts**3.0))
     total = 0.0
@@ -64,63 +64,53 @@ def my_allregions_pT(pressure, temperature):
     my_1 = math.exp(rhos*total)
     return my_0*my_1*0.000055071
 
-#Rem
-#Rem Private Function my_AllRegions_ph(ByVal p As Double, ByVal h As Double) As Double
-#Rem   Dim h0, h1, h2, h3, h4, h5, h6 As Variant, rho, T, Ts, ps, my0, sum, my1, rhos, v4V, v4L, xs As Double, i As Integer
-#Rem   h0 = Array(0.5132047, 0.3205656, 0, 0, -0.7782567, 0.1885447)
-#Rem   h1 = Array(0.2151778, 0.7317883, 1.241044, 1.476783, 0, 0)
-#Rem   h2 = Array(-0.2818107, -1.070786, -1.263184, 0, 0, 0)
-#Rem   h3 = Array(0.1778064, 0.460504, 0.2340379, -0.4924179, 0, 0)
-#Rem   h4 = Array(-0.0417661, 0, 0, 0.1600435, 0, 0)
-#Rem   h5 = Array(0, -0.01578386, 0, 0, 0, 0)
-#Rem   h6 = Array(0, 0, 0, -0.003629481, 0, 0)
-#Rem
-#Rem   'Calcualte density.
-#Rem  Select Case region_ph(p, h)
-#Rem  Case 1
-#Rem    Ts = T1_ph(p, h)
-#Rem    T = Ts
-#Rem    rho = 1 / v1_pT(p, Ts)
-#Rem  Case 2
-#Rem    Ts = T2_ph(p, h)
-#Rem    T = Ts
-#Rem    rho = 1 / v2_pT(p, Ts)
-#Rem  Case 3
-#Rem    rho = 1 / v3_ph(p, h)
-#Rem    T = T3_ph(p, h)
-#Rem  Case 4
-#Rem    xs = x4_ph(p, h)
-#Rem    If p < 16.529 Then
-#Rem      v4V = v2_pT(p, T4_p(p))
-#Rem      v4L = v1_pT(p, T4_p(p))
-#Rem    Else
-#Rem      v4V = v3_ph(p, h4V_p(p))
-#Rem      v4L = v3_ph(p, h4L_p(p))
-#Rem     End If
-#Rem     rho = 1 / (xs * v4V + (1 - xs) * v4L)
-#Rem     T = T4_p(p)
-#Rem  Case 5
-#Rem    Ts = T5_ph(p, h)
-#Rem    T = Ts
-#Rem    rho = 1 / v5_pT(p, Ts)
-#Rem  Case Else
-#Rem   my_AllRegions_ph = CVErr(xlErrValue)
-#Rem   Exit Function
-#Rem  End Select
-#Rem   rhos = rho / 317.763
-#Rem   Ts = T / 647.226
-#Rem   ps = p / 22.115
-#Rem   'Check valid area
-#Rem   If T > 900 + 273.15 Or (T > 600 + 273.15 And p > 300) Or (T > 150 + 273.15 And p > 350) Or p > 500 Then
-#Rem     my_AllRegions_ph = CVErr(xlErrValue)
-#Rem     Exit Function
-#Rem   End If
-#Rem   my0 = Ts ^ 0.5 / (1 + 0.978197 / Ts + 0.579829 / (Ts ^ 2) - 0.202354 / (Ts ^ 3))
-#Rem
-#Rem   sum = 0
-#Rem   For i = 0 To 5
-#Rem       sum = sum + h0(i) * (1 / Ts - 1) ^ i + h1(i) * (1 / Ts - 1) ^ i * (rhos - 1) ^ 1 + h2(i) * (1 / Ts - 1) ^ i * (rhos - 1) ^ 2 + h3(i) * (1 / Ts - 1) ^ i * (rhos - 1) ^ 3 + h4(i) * (1 / Ts - 1) ^ i * (rhos - 1) ^ 4 + h5(i) * (1 / Ts - 1) ^ i * (rhos - 1) ^ 5 + h6(i) * (1 / Ts - 1) ^ i * (rhos - 1) ^ 6
-#Rem   Next i
-#Rem   my1 = Exp(rhos * sum)
-#Rem   my_AllRegions_ph = my0 * my1 * 0.000055071
-#Rem End Function
+def my_allregions_ph(pressure, enthalpy):
+    '''Viscosity (IAPWS formulation 1985, Revised 2003)'''
+    region = Regions.region_ph(pressure, enthalpy)
+    if region is None: return Constants._errorValue
+    
+    temperature = Constants._errorValue
+    density = Constants._errorValue
+
+    if region == 1:
+        temperature = Region1.t1_ph(pressure, enthalpy)
+        density = 1.0/Region1.v1_pt(pressure, temperature)
+    elif region == 2:
+        temperature = Region2.t2_ph(pressure, enthalpy)
+        density = 1.0/Region2.v2_pt(pressure, temperature)
+    elif region == 3:
+        temperature = Region3.t3_ph(pressure, enthalpy)
+        density = 1.0/Region3.v3_ph(pressure, enthalpy)
+    elif region == 4:
+        quality = Region4.x4_ph(pressure, enthalpy)
+        specificVolumeVapor, specificVolumeLiquid = Constants._errorValue, Constants._errorValue
+        temperature = Region4.t4_p(pressure)
+        if pressure < Constants._pressureSubDomain:
+            specificVolumeVapor = Region2.v2_pt(pressure, temperature)
+            specificVolumeLiquid = Region1.v1_pt(pressure, temperature)
+        else:
+            specificVolumeVapor = Region3.v3_ph(pressure, Region4.h4_p(pressure, 'vap'))
+            specificVolumeLiquid = Region3.v3_ph(pressure, Region4.h4_p(pressure, 'liq'))
+        density = 1.0/(quality*specificVolumeVapor + (1.0 - quality)*specificVolumeLiquid)
+    elif region == 5:
+        temperature = Region5.t5_ph(pressure, enthalpy)
+        density = 1.0/Region5.v5_pt(pressure, temperature)
+
+    # Check valid area
+    if temperature > 900.0 + 273.15 or \
+       (temperature > 600.0 + 273.15 and pressure > 300.0) or \
+       (temperature > 150.0 + 273.15 and pressure > 350.0) or \
+       pressure > 500.0:
+       return Constants._errorValue
+
+    rhos = density/317.63
+    ts = temperature/647.226
+
+    my_0 = ts**0.5/(1.0 + 0.978197/ts + 0.579829/(ts**2.0) - 0.202354/(ts**3.0))
+    total = 0.0
+    a, b = 1.0/ts - 1.0, rhos - 1.0
+    for i in range(6):
+        total += h0[i]*a**i + h1[i]*a**i*b + h2[i]*a**i*b**2 + h3[i]*a**i*b**3 + h4[i]*a**i*b**4 + h5[i]*a**i*b**5 + h6[i]*a**i*b**6
+
+    my_1 = math.exp(rhos*total)
+    return my_0*my_1*0.000055071
--- tests/Viscosity_Tests.py
+++ tests/Viscosity_Tests.py
@@ -18,26 +18,49 @@ import Viscosity
 
 class Test_my_AllRegions_pT(unittest.TestCase):
 
-    def test_my_pT_region1(self):
+    def test_my_allregions_pT_region1(self):
         self.assertAlmostEqual(Viscosity.my_allregions_pT(1.0, 300.0),0.0008535, places=7)
 
-    def test_my_pT_region2(self):
+    def test_my_allregions_pT_region2(self):
         self.assertAlmostEqual(Viscosity.my_allregions_pT(1.0, 500.0), 1.71e-5, places=7)
 
-    def test_my_pT_region3(self):
+    def test_my_allregions_pT_region3(self):
         self.assertAlmostEqual(Viscosity.my_allregions_pT(19.0, 640.0), 2.54e-5, places=7)
 
-    def test_my_pT_region4(self):
+    def test_my_allregions_pT_region4(self):
         self.assertAlmostEqual(Viscosity.my_allregions_pT(2.63890, 500.0), 0.0, places=7)
 
-    def test_my_pT_region5(self):
+    def test_my_allregions_pT_region5(self):
         self.assertAlmostEqual(Viscosity.my_allregions_pT(9.0, 1100.0), 4.18e-5, places=7)
 
-    def test_my_pT_invalid_area(self):
+    def test_my_allregions_pT_invalid_area(self):
         self.assertEqual(Viscosity.my_allregions_pT(9.0, 2000.0), 2015.0)
         self.assertEqual(Viscosity.my_allregions_pT(350.0, 900.0), 2015.0)
         self.assertEqual(Viscosity.my_allregions_pT(400.0, 500.0), 2015.0)
         self.assertEqual(Viscosity.my_allregions_pT(600.0, 300.0), 2015.0)
+
+class Test_my_AllRegions_ph(unittest.TestCase):
+
+    def test_my_allregions_ph_region1(self):
+        self.assertAlmostEqual(Viscosity.my_allregions_ph(1.0, 100.0), 0.000918, places=6)
+
+    def test_my_allregions_ph_region2(self):
+        self.assertAlmostEqual(Viscosity.my_allregions_ph(1.0, 4000.0), 3.79e-5, places=7)
+    
+    def test_my_allregions_ph_region3(self):
+        self.assertAlmostEqual(Viscosity.my_allregions_ph(19.0, 2500.0), 2.58e-5, places=7)
+    
+    def test_my_allregions_ph_region4(self):
+        self.assertAlmostEqual(Viscosity.my_allregions_ph(1.0, 1000.0), 1.33e-5, places=7)
+
+    def test_my_allregions_ph_region5(self):
+        self.assertAlmostEqual(Viscosity.my_allregions_ph(1.0, 4250.0), 4.19e-5, places=7)
+
+    def test_my_ph_invalid_area(self):
+        self.assertEqual(Viscosity.my_allregions_ph(9.0, 6590.0), 2015.0)
+        self.assertEqual(Viscosity.my_allregions_ph(350.0, 3490.0), 2015.0)
+        self.assertEqual(Viscosity.my_allregions_ph(400.0, 990.0), 2015.0)
+        self.assertEqual(Viscosity.my_allregions_ph(600.0, 170.0), 2015.0)
 
 if __name__ == '__main__':
     unittest.main()
